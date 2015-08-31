14e1241f6454bb2a351d542e16c228e6c1ac17fa
Chris Scribner <scriby@gmail.com>
2015-08-31 17:01:08 -0400

    Fix up dist files

---
+++ dist/protractor_sync.d.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../../node_modules/node-shared-typescript-defs/angular-protractor-sync/angular-protractor-sync.d.ts" />
+/// <reference path="../../../node_modules/node-shared-typescript-defs/asyncblock/asyncblock.d.ts" />
+/// <reference path="../../../node_modules/node-shared-typescript-defs/node/node.d.ts" />
+export declare module protractor_sync {
+    var IMPLICIT_WAIT_MS: number;
+    var RETRY_INTERVAL: number;
+    /**
+     * Apply synchronous patches to protractor
+     */
+    function patch(): void;
+    var disallowMethods: () => void;
+    function injectjQuery(): void;
+}
